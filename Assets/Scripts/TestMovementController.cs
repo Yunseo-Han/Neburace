d6cc96b3d79d2b96a1c37e6914e291c86a759ff9
Yunseo Han <yunseohan@Yunseos-MacBook-Pro.local>
2022-03-10 17:19:44 -0800

    Added track physics

---
+++ Assets/Scripts/TestMovementController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class TestMovementController : MonoBehaviour
+{
+    Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            playerRB.AddForce(Vector3.right);
+            //transform.position += Vector3.right;
+        } else if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            playerRB.AddForce(Vector3.left);
+            //transform.position += Vector3.left;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/TrackPhysicsController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPhysicsController : MonoBehaviour
+{
+    Collider trackBounds;
+    [SerializeField] private float thrust = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackBounds = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        GameObject curr = collider.gameObject;
+        Rigidbody ObjectRigidbody = curr.GetComponent<Rigidbody>();
+        Vector3 direction = ObjectRigidbody.velocity.normalized;
+        Vector3 newforce = direction * thrust;
+        ObjectRigidbody.AddForce(newforce, ForceMode.VelocityChange);
+
+    }
+
+
+}
